081103987851da72669a5cc13c1ff9378eea059b
zhengjian <570470242@qq.com>
2024-06-16 00:24:54 +0800

    array insert

--- helloalgo/src/com/zhengj/hello/ArrayTest.java
+++ helloalgo/src/com/zhengj/hello/ArrayTest.java
@@ -1,5 +1,6 @@
 package com.zhengj.hello;
 
+import java.util.Arrays;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class ArrayTest {
@@ -7,7 +8,8 @@ public class ArrayTest {
     public static void main(String[] args) {
         int[] nums = {1,3,5,6,9,11,212,5678224};
         System.out.println(randomAccess(nums));
-
+        insert(nums,99,2);
+        System.out.println(Arrays.toString(nums));
     }
 
     private static int randomAccess(int[] nums) {
@@ -16,5 +18,13 @@ public class ArrayTest {
         System.out.println(randomIndex);
         int randomNum = nums[randomIndex];
         return randomNum;
+    }
+
+    private static void insert(int[] nums,int num,int index) {
+
+        for(int i = nums.length -1 ; i > index; i--) {
+            nums[i] = nums[i-1];
+        }
+        nums[index] = num;
     }
 }

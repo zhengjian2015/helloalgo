71019e8e5b712d8095c7b9b5d27ff6f79eea2d11
zhengjian <570470242@qq.com>
2024-06-16 00:32:44 +0800

    array del

--- helloalgo/src/com/zhengj/hello/ArrayTest.java
+++ helloalgo/src/com/zhengj/hello/ArrayTest.java
@@ -6,25 +6,47 @@ import java.util.concurrent.ThreadLocalRandom;
 public class ArrayTest {
 
     public static void main(String[] args) {
-        int[] nums = {1,3,5,6,9,11,212,5678224};
+        int[] nums = {1, 3, 5, 6, 9, 11, 212, 5678224};
         System.out.println(randomAccess(nums));
-        insert(nums,99,2);
+        insert(nums, 99, 2);
+        System.out.println(Arrays.toString(nums));
+        remove(nums, 5);
         System.out.println(Arrays.toString(nums));
     }
 
     private static int randomAccess(int[] nums) {
 
-        int randomIndex = ThreadLocalRandom.current().nextInt(0,nums.length);
+        int randomIndex = ThreadLocalRandom.current().nextInt(0, nums.length);
         System.out.println(randomIndex);
         int randomNum = nums[randomIndex];
         return randomNum;
     }
 
-    private static void insert(int[] nums,int num,int index) {
+    private static void insert(int[] nums, int num, int index) {
 
-        for(int i = nums.length -1 ; i > index; i--) {
-            nums[i] = nums[i-1];
+        for (int i = nums.length - 1; i > index; i--) {
+            nums[i] = nums[i - 1];
         }
         nums[index] = num;
+    }
+
+    static void remove(int[] nums, int index) {
+        // 把索引 index 之后的所有元素向前移动一位
+        for (int i = index; i < nums.length - 1; i++) {
+            nums[i] = nums[i + 1];
+        }
+    }
+
+    /* 遍历数组 */
+    void traverse(int[] nums) {
+        int count = 0;
+        // 通过索引遍历数组
+        for (int i = 0; i < nums.length; i++) {
+            count += nums[i];
+        }
+        // 直接遍历数组元素
+        for (int num : nums) {
+            count += num;
+        }
     }
 }
